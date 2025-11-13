48bc2fac9d92a169c4475eba41884d615e7dc276
tranhuong0204 <huongtran4002@gmail.com>
2025-11-14 00:34:21 +0700

    tạo module

--- src/main/java/com/example/onlineCourses/repository/ProviderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.onlineCourses.repository;
-
-import com.example.onlineCourses.model.Provider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProviderRepository extends JpaRepository<Provider, Long> {
-    // Bạn có thể thêm các phương thức tìm kiếm tùy chỉnh ở đây nếu cần
-    Provider findByName(String name);
-}
